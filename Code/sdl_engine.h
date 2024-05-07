42e869182183c87e027c1c3d0297600a93a415eb
DDung <150952695+DugnCon@users.noreply.github.com>
2024-05-07 16:26:26 +0700

    sdl_engine.h

--- Code/sdl_engine.h
+++ Code/sdl_engine.h
@@ -16,6 +16,6 @@ void loadSprites(SDL_Surface*& surface, SDL_Texture*& texture, SDL_Renderer*& re
 bool collisionCheck(SDL_Rect& rect1, SDL_Rect& rect2);
 bool mouseInRect(SDL_Rect& rect, int x, int y);
 bool mouseParRect(SDL_Rect& rect, int y);
-void framerateControl(int& frameNum);
+void framerateControl(int& frameNum);// dieu khien toc do khung hinh
 
 #endif
