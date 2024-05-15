8963f685f92f7b0918f226894d9545453db8afeb
DDung <150952695+DugnCon@users.noreply.github.com>
2024-05-15 19:57:13 +0700

    background.cpp

--- Code/background.cpp
+++ Code/background.cpp
@@ -3,21 +3,20 @@
 using namespace std;
 
 void Background::select() {
-    srand(time(NULL));
-    int selection = rand() % 2 + 1;
-    if (selection == 1) path = "bacck_gr.png";
-    else if (selection == 2) path = "nightBackground.png";
+    //srand(time(NULL));
+    path = "bacck_gr.png";
 }
 
 void Background::init(SDL_Renderer* renderer) {
     select();
     loadSprites(surface, texture, renderer, path);
-    rect1 = { 0,-120,1280,720 };
-    rect2 = { 1280, -120, 1280, 720 };
+    rect1 = { 0,-100,1280,720 };
+    rect2 = { 1280, -100, 1280, 720 };
     //music = new SFX;
     //music->init()
 }
 //tinh khung hinh cho anh toa do x , y , he so nhan khung hinh
+// di chuyển của nền hay background
 void Background::display(SDL_Renderer* renderer, int multiplier) {
     SDL_RenderCopy(renderer, texture, NULL, &rect1);
     SDL_RenderCopy(renderer, texture, NULL, &rect2);
